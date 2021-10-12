04fbf8dfb8b17fd3b17c14606ef2955e4da1e5d0
Brian Anderson <andersrb@gmail.com>
2021-10-11 21:49:49 -0600

    .

--- src/solana-accounts/src/lib.rs
+++ src/solana-accounts/src/lib.rs
@@ -1,13 +1,25 @@
 use solana_program::pubkey::Pubkey as SolanaPubkey;
 
 
-
 pub type Pubkey = SolanaPubkey;
 
 pub struct Signer<K>(K) where K: Accounts;
 
 pub trait Accounts {
+    fn pubkeys(&self) -> Vec<Pubkey>;
+    fn signers(&self) -> Vec<Pubkey>;
 }
 
 impl Accounts for Pubkey {
+    fn pubkeys(&self) -> Vec<Pubkey> { vec![*self] }
+    fn signers(&self) -> Vec<Pubkey> { vec![] }
+}
+
+impl<K> Accounts for Signer<K> where K: Accounts {
+    fn pubkeys(&self) -> Vec<Pubkey> {
+        self.0.pubkeys()
+    }
+    fn signers(&self) -> Vec<Pubkey> {
+        self.0.pubkeys()
+    }
 }
