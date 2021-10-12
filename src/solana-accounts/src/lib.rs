9f3a6a7b3b31090290360bd68b9b8018ae5b9570
Brian Anderson <andersrb@gmail.com>
2021-10-11 22:09:25 -0600

    .

--- src/solana-accounts/src/lib.rs
+++ src/solana-accounts/src/lib.rs
@@ -5,10 +5,39 @@ pub type Pubkey = SolanaPubkey;
 
 pub struct Signer<K>(K) where K: Account;
 
+pub struct Payer<K>(K) where K: Account;
+
 pub trait Account {
     fn pubkey(&self) -> SolanaPubkey;
     fn signer(&self) -> bool;
     fn writable(&self) -> bool;
     fn owner(&self) -> Option<SolanaPubkey>;
     fn executable(&self) -> bool;
+
+    fn role_payer(&self) -> bool;
+}
+
+impl Account for Pubkey {
+    fn pubkey(&self) -> SolanaPubkey { *self }
+    fn owner(&self) -> Option<SolanaPubkey> { None }
+
+    fn signer(&self) -> bool { false }
+    fn writable(&self) -> bool { false }
+    fn executable(&self) -> bool { false }
+
+    fn role_payer(&self) -> bool { false }
+}
+
+pub struct AccountsDesc {
+    pub payer: SolanaPubkey,
+}
+
+
+
+pub struct AppInstructionAccounts {
+    pub payer: Payer<Pubkey>,
+    pub lib_accounts: LibInstructionAccounts,
+}
+
+pub struct LibInstructionAccounts {
 }
