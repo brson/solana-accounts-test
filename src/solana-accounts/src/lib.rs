d3e0dd4f8b851320a5f5ee9a670af8f89ea9b8af
Brian Anderson <andersrb@gmail.com>
2021-10-12 21:52:11 -0600

    .

--- src/solana-accounts/src/lib.rs
+++ src/solana-accounts/src/lib.rs
@@ -7,19 +7,45 @@ use variant_count::VariantCount;
 
 pub const MAX_CONSTRAINTS: usize = 10;
 
+pub enum TypeOrConstraint {
+    Type(Type),
+    Constraint(Constraint),
+}
+
+pub enum Type {
+    Pubkey,
+    PdaString1(&'static str, Pubkey),
+}
+
+pub enum Constraint {
+    Payer,
+    Signer,
+    Writable,
+    Executable
+}
+
 #[repr(usize)]
 #[derive(VariantCount)]
 pub enum AccountIndex {
-    PrimaryKey,
+    Payer,
+    StorageRef,
+    Storage,
+    NextStorage,
 }
 
-pub enum Constraint { }
+#[repr(usize)]
+#[derive(VariantCount)]
+pub enum KeyIndex {
+    Payer,
+}
 
-pub const fn make_account_list_constraints() -> [(AccountIndex, Constraint); 0] {
+pub const fn make_account_list_constraints() -> [(AccountIndex, TypeOrConstraint); 0] {
     []
 }
 
-pub const fn derive_account_list<const N: usize>() -> [Pubkey; N] {
+pub const fn derive_account_list<const N: usize>(
+    constraints: &[(AccountIndex, Constraint)],
+) -> [Pubkey; N] {
     let mut empty = [Pubkey::new_from_array([0; 32]); N];
     empty
 }
