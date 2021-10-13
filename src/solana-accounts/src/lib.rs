ccf8d5b0fc3ceb443a1c5249d83bfdb0659b374e
Brian Anderson <andersrb@gmail.com>
2021-10-12 21:35:24 -0600

    .

--- src/solana-accounts/src/lib.rs
+++ src/solana-accounts/src/lib.rs
@@ -3,11 +3,12 @@
 mod oldtry;
 
 use solana_program::pubkey::Pubkey;
-
+use variant_count::VariantCount;
 
 pub const MAX_CONSTRAINTS: usize = 10;
 
 #[repr(usize)]
+#[derive(VariantCount)]
 pub enum AccountIndex {
     PrimaryKey,
 }
@@ -18,3 +19,7 @@ pub const fn make_account_list_constraints() -> [(AccountIndex, Constraint); 0] 
     []
 }
 
+pub const fn derive_account_list<const N: usize>() -> [Pubkey; N] {
+    let mut empty = [Pubkey::new_from_array([0; 32]); N];
+    empty
+}
