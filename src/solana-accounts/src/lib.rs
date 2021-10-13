4503d0ebd2c6dc46cabbe4541ff1a8652abfdad4
Brian Anderson <andersrb@gmail.com>
2021-10-13 16:58:37 -0600

    .

--- src/solana-accounts/src/lib.rs
+++ src/solana-accounts/src/lib.rs
@@ -42,7 +42,7 @@ pub enum MyKeyIndex {
     Payer,
 }
 
-pub const fn make_account_list_constraints() -> [(MyAccountIndex, TypeOrConstraint); 0] {
+pub const fn make_my_account_list_constraints() -> [(MyAccountIndex, TypeOrConstraint); 0] {
     []
 }
 
@@ -51,9 +51,42 @@ pub const fn check_constraint_well_formedness(
     num_accounts: usize,
 ) -> bool {
     let mut constraint_index = 0;
+    let mut current_account_index = 0;
 
     loop {
         let (account_index, constraint) = constraints[constraint_index];
+
+        if constraint_index == 0 {
+            if account_index != 0 {
+                return false;
+            }
+        }
+
+        if account_index > current_account_index + 1 {
+            return false;
+        }
+
+        if account_index == current_account_index + 1 {
+            match constraint {
+                TypeOrConstraint::Type(_) => {
+                }
+                TypeOrConstraint::Constraint(_) => {
+                    return false;
+                }
+            }
+        }
+
+        if account_index == current_account_index {
+            match constraint {
+                TypeOrConstraint::Type(_) => {
+                    return false;
+                }
+                TypeOrConstraint::Constraint(_) => {
+                }
+            }
+        }
+
+        current_account_index = account_index;
 
         constraint_index += 1;
         if constraint_index >= constraints.len() {
